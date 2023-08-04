635f1e0ea1b07a9b53cbde7e2a4ace3a44ac1489
Tom_Court <tomcourt94@icloud.com>
2023-08-04 16:20:03 +0100

    basic layout done

--- deployed-dad-joke-generator/src/App.js
+++ deployed-dad-joke-generator/src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
-function App() {
+const Joke = ({ isVisible, onHideJoke, savedJokes, setSavedJokes }) => {
+  const [joke, setJoke] = useState("");
+  const [slideIn, setSlideIn] = useState(false);
+
+  useEffect(() => {
+    // When isVisible prop changes, trigger the slide animation
+    setSlideIn(isVisible);
+  }, [isVisible]);
+
+  const generateJoke = () => {
+    fetch("https://icanhazdadjoke.com/", {
+      headers: {
+        Accept: "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Joke data:", data); // Debugging statement
+        setJoke(data.joke);
+      })
+      .catch((error) => console.log(error));
+  };
+
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`joke-page-content`}>
+      <h2>Dad Joke Generator</h2>
+      <p>
+        Using a 3rd party API, a random 'Dad Joke' is generated on the screen
+        for the user
+      </p>
+      <p>
+        Press the <span className="button-highlight">BUTTON</span> below to
+        randomly generate a thigh-slapping cracker straight to your screen.
+        <br />
+      </p>
+      <button className="joke-button" onClick={generateJoke}>
+        Joke Button
+      </button>
+      <br />
+      {joke && (
+        <div className="joke-container">
+          <p className="joke-text">{joke}</p>
+          <br />
+        </div>
+      )}
     </div>
   );
-}
+};
 
-export default App;
+export default Joke;
